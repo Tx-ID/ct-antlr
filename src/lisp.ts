e6b661b51ba79e51647b505db3b68f6d225b285f
Tix <tix.personal@gmail.com>
2023-12-18 16:41:46 +0700

    lips.ts

--- src/lisp.ts
+++ src/lisp.ts
@@ -3,13 +3,31 @@ const input = 'jadi plis buka plis tambah plis buka plis kurang 70 69 plis tutup
 
 // do not edit below unless you know what you are doing.
 
-import { CharStreams, CommonTokenStream } from 'antlr4ts';
+import { BailErrorStrategy, CharStreams, CommonTokenStream, ANTLRErrorListener, Recognizer, RecognitionException } from 'antlr4ts';
 import { ParseTreeWalker } from 'antlr4ts/tree/ParseTreeWalker'
 
 import { SimpleLISPListener } from './antlr/SimpleLISP/SimpleLISPListener';
 import { ExpressionContext, ProgramContext, SimpleLISPParser } from './antlr/SimpleLISP/SimpleLISPParser';
 import { SimpleLISPLexer } from './antlr/SimpleLISP/SimpleLISPLexer';
 import { TerminalNode } from 'antlr4ts/tree/TerminalNode';
+import { ErrorNode } from 'antlr4ts/tree/ErrorNode';
+
+class MyErrorListener implements ANTLRErrorListener {
+    err?: string
+
+    syntaxError(
+        recognizer: Recognizer,
+        offendingSymbol: any,
+        line: number,
+        charPositionInLine: number,
+        msg: string,
+        e: RecognitionException | undefined
+    ) {
+        // Handle the error here, you can log it or send it to the client
+        // console.error(`Error at line ${line}:${charPositionInLine} - ${msg}`);
+        this.err = msg;
+    }
+}
 
 class lispListener implements SimpleLISPListener {
     result: Array<string> = [];
@@ -38,16 +56,23 @@ class lispListener implements SimpleLISPListener {
         }
     }
 
+    visitErrorNode(node: ErrorNode) {
+        // console.log("error!")
+    }
+
     getResult(): string {
         return this.result.join(" ");
     }
 }
 
-export default function JakselToLISP(input: string): string {
+export default function JakselToLISP(input: string): { success: true, content: string } | { success: false, error: string } {
     const inputStream = CharStreams.fromString(input);
     const lexer = new SimpleLISPLexer(inputStream);
     const tokenStream = new CommonTokenStream(lexer);
     const parser = new SimpleLISPParser(tokenStream);
+
+    const errListener = new MyErrorListener();
+    parser.addErrorListener(errListener);
 
     const listener = new lispListener();
     const tree = parser.program();
@@ -55,8 +80,12 @@ export default function JakselToLISP(input: string): string {
     const walker = new ParseTreeWalker();
     walker.walk(listener, tree);
 
-    return listener.getResult();
+    if (errListener.err !== undefined) {
+        return { success: false, error: errListener.err };
+    }
+    return { success: true, content: listener.getResult() };
 }
 
 const result = JakselToLISP(input);
-console.log(result);
+console.log(result.success);
+console.log(result.success === true ? result.content : result.error);
