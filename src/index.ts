c5b589f0c183595b4dc5772f9988ae219ffe6b1f
istamarahsan <istamarahsan@gmail.com>
2023-12-19 16:58:57 +0700

    removed extra padding within output

--- src/index.ts
+++ src/index.ts
@@ -55,7 +55,7 @@ class Listener implements JakselLISPListener {
     }
 
     getResult(): string {
-        return this.result.join(" ");
+        return this.result.join(" ").replaceAll('( ', '(').replaceAll(' )', ')');
     }
 }
 
